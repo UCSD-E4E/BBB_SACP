82a00ddd03a7244751ea695c124250506942cc7d
Nathan Hui <ntlhui@gmail.com>
2014-05-15 04:02:50 +0000

    Added MPU9150.h.  Began defining register names
    	new file:   MPU9150.h

---
+++ src/MPU9150.h
@@ -0,0 +1,19 @@
+// Gyroscope and Accelerometer Registers
+#define SELF_TEST_X	0x0D
+#define SELF_TEST_Y	0x0E
+#define SELF_TEST_Z	0x0F
+#define SELF_TEST_A	0x10
+#define SMPLRT_DIV	0x19
+#define CONFIG		0x1A
+#define GYRO_CONFIG	0x1B
+#define ACCEL_CONFIG	0x1C
+#define FF_THR		0x1D
+#define FF_DUR		0x1E
+#define MOT_THR		0x1F
+#define	MOT_DUR		0x20
+#define ZRMOT_THR	0x21
+#define ZRMOT_DUR	0x22
+#define FIFO_EN		0x23
+#define I2C_MST_CTRL	0x24
+#define I2C_SLV0_ADDR	0x25
+
