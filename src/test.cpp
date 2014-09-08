777571f13ab6526f12550b473e6f3d6108e204e7
Nathan <ntlhui@gmail.com>
2014-09-08 11:54:28 -0700

    Added constant polling code in the test routine.  Removed the calibration call.  Will begin testing to identify hardware issues

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,6 @@
 #include "MPU9150.hpp"
 #include <iostream>
+#include <cstdio>
 #include <cmath>
 using namespace std;
 
@@ -21,15 +22,25 @@ int main(){
 	magnitude = sqrt(magnitude);
 	cout << magnitude << endl;
 	
-	if(!sensor.calibrate()){
-		return 1;
+	// if(!sensor.calibrate()){
+		// return 1;
+	// }
+	// sensor.getSensorState();
+	// cout << sensor.getAccelX() << endl;
+	// cout << sensor.getAccelY() << endl;
+	// cout << sensor.getAccelZ() << endl;
+	// magnitude = sensor.getAccelX() * sensor.getAccelX() + sensor.getAccelY() * sensor.getAccelY() + sensor.getAccelZ() * sensor.getAccelZ();
+	// magnitude = sqrt(magnitude);
+	// cout << magnitude << endl;
+	
+	// Print out sensor data
+	while(1){
+		if(sensor.getSensorState()){
+			return 1;
+		}
+		magnitude = sensor.getAccelX() * sensor.getAccelX() + sensor.getAccelY() * sensor.getAccelY() + sensor.getAccelZ() * sensor.getAccelZ();
+		magnitude = sqrt(magnitude);
+		printf("%.3f\t$.3f\t%.3f\t%.3f\n", sensor.getAccelX(), sensor.getAccelY(), sensor.getAccelZ(), magnitude);
 	}
-	sensor.getSensorState();
-	cout << sensor.getAccelX() << endl;
-	cout << sensor.getAccelY() << endl;
-	cout << sensor.getAccelZ() << endl;
-	magnitude = sensor.getAccelX() * sensor.getAccelX() + sensor.getAccelY() * sensor.getAccelY() + sensor.getAccelZ() * sensor.getAccelZ();
-	magnitude = sqrt(magnitude);
-	cout << magnitude << endl;
 	return 0;
 }
