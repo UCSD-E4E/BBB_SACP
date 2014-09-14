9dc38d4a8b319b71b5c12f3a0fc0c0fffd7e155a
Nathan Hui <ntlhui@gmail.com>
2014-09-14 10:04:55 -0700

    Added ZMQ context creation code and sensor initialization code

--- src/BBB_Control.cpp
+++ src/BBB_Control.cpp
@@ -6,10 +6,13 @@
 #include <zmq.hpp>
 #include "quaternion.hpp"
 #include "MPU9150.hpp"
+#include "dataStructure.hpp"
+using namespace std;
 
 // Defines
 
 // Global Variables
+
 
 int main(int argc, char** argv){
 	// Set up communications
@@ -18,7 +21,21 @@ int main(int argc, char** argv){
 	zmq::socket_t control_Socket (control_Context, ZMQ_REP);
 	control_Socket.bind("tcp://*:53679");
 
+	zmq::context_t data_Context (1);
+	zmq::socket_t data_Socket (data_Context, ZMQ_PUB);
+	data_Socket.bind("tcp://eth0:53680");
+
 	// Set up sensors
+	MPU9150 sensor (1);
+	int result = sensor.initialize();
+	if(result){
+		return 1;
+	}
+
+	if((result = getSensorState())){
+		return 1;
+	}
+
 
 	// Set up stabilization
 
