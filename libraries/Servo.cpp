95e905026c226c51613239ee74dfd6e09aae7124
Nathan Hui <ntlhui@gmail.com>
2014-10-17 11:13:25 -0700

    Changed constructor and angle referencing to measure angle from low_pulse

--- libraries/Servo.cpp
+++ libraries/Servo.cpp
@@ -1,14 +1,17 @@
 #include "Servo.hpp"
+#include <iostream>
+using namespace std;
+
 
 Servo::Servo(const pwm_pins_t pin, const uint32_t frequency, 
 		const uint32_t lowPulse, const uint32_t highPulse, 
-		const float msDegRatio):_pwmPin(pin, frequency), _lowPulse(lowPulse), 
-		_highPulse(highPulse), _pulseScaling(msDegRatio), _curPulse(15000000){
+		const int rotateRange):_pwmPin(pin, frequency), _lowPulse(lowPulse), 
+		_highPulse(highPulse), _range(rotateRange), _curPulse(15000000){
 	_pwmPin.setPolarity(PWM_PULSE_HIGH);
 }
 
 void Servo::setAngle(const float degrees){
-	uint32_t curPulse = degrees * _pulseScaling + (_lowPulse + _highPulse) / 2;
+	uint32_t curPulse = degrees / _range * (_highPulse - _lowPulse) + _lowPulse;
 	assert(curPulse >= _lowPulse && curPulse <= _highPulse);
 	_curPulse = curPulse;
 	_curLocation = degrees;
