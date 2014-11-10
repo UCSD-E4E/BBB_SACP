ff32b9eee9cf03e281cedf8e37d5627cdba835b8
ntlhui <ntlhui@gmail.com>
2014-11-09 20:55:33 -0800

    Testing stdin usage

--- test/testAVRUART.c
+++ test/testAVRUART.c
@@ -9,8 +9,15 @@ int main(int argc, char** argv){
 	uart_init();
 	FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 	stdout = stdin = &uart_str;
+	int roll, pitch, yaw;
+	roll = 0;
+	pitch = 0;
+	yaw = 0;
 	while(1){
-		printf("Hello World!\n");
 		_delay_ms(500);
+		if(UCSR0A & (1 << RXC0)){
+			scanf("%d %d %d", &roll, &pitch, &yaw);
+			printf("%d\t%d\t%d\n", roll, pitch, yaw);
+		}
 	}
 }
