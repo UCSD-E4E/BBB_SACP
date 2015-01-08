a71b9e78fc8c57c7672005f37ebfa0ca5abac2bb
ntlhui <ntlhui@gmail.com>
2015-01-07 22:14:56 -0800

    added new test code.  Quaternion support deprecated!

--- test/testQuat.c
+++ test/testQuat.c
@@ -5,21 +5,24 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <ctype.h>
+#include <math.h>
 
 FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 
 int main(int argc, char** argv){
 	uart_init();
 	stdout = stdin = &uart_str;
-	float quat1[4] = {.707107, .707107, 0, 0};
-	float quat2[4] = {.707107, 0, .707107, 0};
-	float quat3[4] = {0};
-	_mulQuat(quat1, quat2, quat3);
 
-	for(int i = 0; i < 4; i++){
-		printf("%3.3f\t", quat3[i]);
-	}
-	printf("\n");
+	float newAcc[3] = {0, 0, 1.001};
+	float _prevAcc[3] = {0, 0, 1};
+	
+	float halfAngAcc = acos(fmin(fmax(_dot(newAcc, _prevAcc), -1.0), 1.0)) / 2;
+
+	printf("%3.3f\t", cos(halfAngAcc));
+	printf("%3.3f\t", (newAcc[1] * _prevAcc[2] - newAcc[2] * _prevAcc[1]) * sin(halfAngAcc));
+	printf("%3.3f\t", (newAcc[2] * _prevAcc[0] - newAcc[0] * _prevAcc[2]) * sin(halfAngAcc));
+	printf("%3.3f\n", (newAcc[0] * _prevAcc[1] - newAcc[1] * _prevAcc[0]) * sin(halfAngAcc));
+
 	while(1){
 		continue;
 	}
