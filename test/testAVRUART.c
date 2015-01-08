0a92faf20341fa462d8004da8973341eb336edf9
ntlhui <ntlhui@gmail.com>
2015-01-07 22:12:07 -0800

    Added blinky program to the end to enable multiple diagnostics

--- test/testAVRUART.c
+++ test/testAVRUART.c
@@ -9,15 +9,15 @@ int main(int argc, char** argv){
 	uart_init();
 	FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 	stdout = stdin = &uart_str;
-	int roll, pitch, yaw;
-	roll = 0;
-	pitch = 0;
-	yaw = 0;
+	printf("Arduino Gimbal Controller v%d.%d, compiled %s at %s\n", 0, 9, __DATE__, __TIME__);
+	double test = 3.1415;
+	printf("Float: %f\n", test);
+	//Initialize
+	DDRB |= (1 << 5);	//configure PB5 as output
+
+	//loop
 	while(1){
-		_delay_ms(500);
-		if(UCSR0A & (1 << RXC0)){
-			scanf("%d %d %d", &roll, &pitch, &yaw);
-			printf("%d\t%d\t%d\n", roll, pitch, yaw);
-		}
+		PORTB ^= (1 << 5);	// toggle PB5
+		_delay_ms(500);	// delay 500 ms
 	}
 }
