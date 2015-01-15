5366d73a1324cb153c1d3794721d401c2cedd3aa
ntlhui <ntlhui@gmail.com>
2015-01-15 00:10:10 -0800

    Initial commit

---
+++ test/calibMag.c
@@ -0,0 +1,33 @@
+/**
+ * Calibration suite for the MPU9150's magnetometer.  The purpose of this
+ * program is to gather the data necessary for the included Matlab scripts to
+ * accurately calculate the calibration matrix for the magnetometer.
+ *
+ * @author Nathan Hui
+ * @date 1/15/2015
+ */
+
+///////////////////
+// Include Files //
+///////////////////
+#include <MPU9150.h>
+
+/////////////
+// Defines //
+/////////////
+
+//////////////////////
+// Global Variables //
+//////////////////////
+
+/////////////////////////
+// Function Prototypes //
+/////////////////////////
+
+//////////////////////////
+// Function Definitions //
+//////////////////////////
+
+int main(int argc, char** argv){
+	
+}
